4fd7a4118f04191b4a86ec7f6a319d1cf21fe73c
egkoppel <egluschovekoppel1@gmail.com>
2023-12-20 21:55:10 +0000

    ok we need use_core

--- lvgl-sys/build.rs
+++ lvgl-sys/build.rs
@@ -226,7 +226,7 @@ fn main() {
         .generate_comments(false)
         .derive_default(true)
         .layout_tests(false)
-        /*.use_core()*/
+        .use_core()
         .ctypes_prefix("fizzbuzz")
         .clang_args(&cc_args)
         .clang_args(&additional_args)
